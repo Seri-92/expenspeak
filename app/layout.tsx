9b88820c9d192831c60576cc99faf25cefeca826
Serita Sohei <sh192b@gmail.com>
2024-11-10 12:52:16 +0900

    feat: 支出一覧へのヘッダーメニュー追加、スタイルの微修正

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,11 @@
 import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+
+const inter = Inter({subsets: ['latin']});
 
 export const metadata: Metadata = {
   title: "Expenspeak",
@@ -13,17 +19,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
-      <body>
-        <header>
-          <div className='p-4'>
-            <h1 className='text-4xl font-bold mb-4'>Expenspeak</h1>
-          </div>
+      <body className={cn(inter.className, 'min-h-dvh')}>
+        <header className="h-16 border-b px-6 flex items-center">
+          <Button asChild variant="ghost" className="font-bold text-xl">
+            <Link href="/"> Expenspeak</Link>
+          </Button>
+          <Button asChild variant="ghost" className="font-bold text-xl">
+            <Link href="/expenses">支出一覧</Link>
+          </Button>
+            {/* <h1 className='text-4xl font-bold mb-4'>Expenspeak</h1> */}
         </header>
         <main>{children}</main>
-        <footer>
-          <div className='p-4'>
-            <p>&copy; {new Date().getFullYear()} Expenspeak</p>
-          </div>
+        <footer className="h-16 sticky top-full border-t px-6 flex items-center">
+          <p>&copy; {new Date().getFullYear()} Expenspeak</p>
         </footer>
       </body>
     </html>
