7623a93cbdf3fdd27445b02b4f2dac9e8a1e2a26
Serita Sohei <sh192b@gmail.com>
2025-01-03 00:01:57 +0900

    feat: 支出一覧を別ページで出す

--- app/expenses/page.tsx
+++ app/expenses/page.tsx
@@ -1,7 +1,31 @@
-import React from 'react'
+"use client";
+import ExpenseListDisplay from '@/components/custom/ExpenseListDisplay'
+import { supabase } from '@/lib/supabaseClient';
+import React, { useEffect, useState } from 'react'
 
 export default function page() {
+  const [expenses, setExpenses] = useState([]);
+
+  useEffect(() => {
+    fetchExpenses();
+  }, []);
+
+  const fetchExpenses = async () => {
+    const { data, error } = await supabase
+      .from("expenses")
+      .select("*")
+      .order("date", { ascending: false });
+
+    if (error) {
+      console.error("Error fetching expenses:", error);
+    } else if (data) {
+      setExpenses(data);
+    }
+  };
   return (
-    <div className='p-6'>支出一覧</div>
+    <div>
+      <h1 className='p-6'>支出一覧</h1>
+      <ExpenseListDisplay expenses={expenses} />
+    </div>
   )
 }
